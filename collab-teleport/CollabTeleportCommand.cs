27c85c97b947e514b5c76a1d586bdaa7fbe263d9
erictran0x <40257345+erictran0x@users.noreply.github.com>
2021-02-03 12:12:08 -0800

    Fixed bug where player tp's to wrong position at lobbies not positioned at (0, 0)

--- collab-teleport/CollabTeleportCommand.cs
+++ collab-teleport/CollabTeleportCommand.cs
@@ -25,7 +25,7 @@ namespace Celeste.Mod.CollabTeleport
             List<EntityData> filteredLevels = CollabTeleportModule.Instance.collabChapters.FindAll(t =>
             {
                 bool success = CollabTeleportModule.Instance.foundAreas.TryGetValue(t.Attr("map"), out AreaStats a);
-                return success /* && !a.SID.Contains($"/0-Gyms/") */ && !a.Modes[0].Completed;
+                return success && !a.SID.Contains($"/0-Gyms/") && !a.Modes[0].Completed;
             });
 
             // Check if there are no noncompleted collab levels left - return if so
@@ -53,13 +53,12 @@ namespace Celeste.Mod.CollabTeleport
                 // Calculate distance if area exists
                 if (CollabTeleportModule.Instance.foundAreas.TryGetValue(name, out AreaStats area))
                 {
-                    // Calculate horizontal distance
-                    float dx = t.Position.X - player.Position.X;
-                    float ddx = Math.Min(Math.Abs(dx), Math.Abs(dx + t.Width));
+                    // Get distance vector
+                    Vector2 diff = t.Position - player.Position + CollabTeleportModule.Instance.currentLevel.LevelOffset;
 
-                    // Calculate vertical distance
-                    float dy = t.Position.Y - player.Position.Y;
-                    float ddy = Math.Min(Math.Abs(dy), Math.Abs(dy + t.Height));
+                    // Calculate min horiz and vert distances
+                    float ddx = Math.Min(Math.Abs(diff.X), Math.Abs(diff.X + t.Width));
+                    float ddy = Math.Min(Math.Abs(diff.Y), Math.Abs(diff.Y + t.Height));
 
                     // Compare current highest dist
                     float dist = (float)(Math.Pow(ddx, 2) + Math.Pow(ddy, 2));
@@ -80,22 +79,20 @@ namespace Celeste.Mod.CollabTeleport
             {
                 for (int i = 0; i <= w; i += 7)  // left to right
                 {
-                    Vector2 v_top = new Vector2(pos.X + i, pos.Y + h/2 - j);
-                    Vector2 v_bot = new Vector2(pos.X + i, pos.Y + h/2 + j);
+                    Vector2 v_top = CollabTeleportModule.Instance.currentLevel.LevelOffset + new Vector2(pos.X + i, pos.Y + h/2 - j);
+                    Vector2 v_bot = CollabTeleportModule.Instance.currentLevel.LevelOffset + new Vector2(pos.X + i, pos.Y + h/2 + j);
                     if (!player.CollideCheck<Solid>(v_top))
                     {
                         // Teleport player to position
-                        Engine.Commands.Log($"Found open spot at {v_top} - teleporting to {nextMap} .");
-                        player.X = v_top.X;
-                        player.Y = v_top.Y;
+                        Engine.Commands.Log($"Teleporting to {nextMap} .");
+                        player.Position = v_top;
                         return;
                     }
                     if (!player.CollideCheck<Solid>(v_bot))
                     {
                         // Teleport player to position
-                        Engine.Commands.Log($"Found open spot at {v_bot} - teleporting to {nextMap} .");
-                        player.X = v_bot.X;
-                        player.Y = v_bot.Y;
+                        Engine.Commands.Log($"Teleporting to {nextMap} .");
+                        player.Position = v_bot;
                         return;
                     }
                 }
